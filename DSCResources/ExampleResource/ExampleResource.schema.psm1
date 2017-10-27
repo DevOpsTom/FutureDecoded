134c42ec85ad3836fc5beeb251c665fc8022b77f
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-10-27 09:27:46 +0100

    Added a module

---
+++ DSCResources/ExampleResource/ExampleResource.schema.psm1
@@ -0,0 +1,21 @@
+configuration ExampleResource
+{
+    param (
+        [ValidateSet('Present', 'Absent')]
+        [string] $Ensure = 'Present'
+     )
+
+    #Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+    #
+    #xRegistry disableInsecureCipher_a
+    #{
+    #    Key       = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\DES 56/56'
+    #    ValueName = 'Enabled'
+    #    ValueData = '0'
+    #    ValueType = 'Dword'
+    #    Ensure    = 'Present'
+    #    Force     = $true
+    #}
+
+}
+
